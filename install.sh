9259356e349459a34aed5524879dc684d528437b
TheAndruNique <theandrusyper@gmail.com>
2023-11-21 20:08:32 +0300

    add installer for project

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root. Exiting..."
+  exit 1
+fi
+
+sudo apt install jq
+
+cp ./scripts/ssh_login.sh /usr/local/bin/ssh_login.sh
+chmod +x /usr/local/bin/ssh_login.sh ./scripts/ssh_login.sh ./scripts/ban_user.sh ./scripts/kick_user.sh
+
+echo "session optional pam_exec.so type=open_session seteuid /usr/local/bin/ssh_login.sh" >> /etc/pam.d/common-session
 
 python -m venv venv
 ./venv/bin/pip install -r requirements.txt
+
+current_python=$(pwd)/venv/bin/python
 
 SERVICE_CONTENT="[Unit]
 Description=server security manager
 
 [Service]
-ExecStart=$(realpath venv/bin/python) $(realpath main.py)
+ExecStart=$current_python $(realpath main.py)
 Restart=always
 WorkingDirectory=$(realpath .)
 
@@ -15,10 +28,6 @@ WorkingDirectory=$(realpath .)
 WantedBy=multi-user.target
 "
 
-if [ "$(id -u)" != "0" ]; then
-  echo "This script must be run as root. Exiting..."
-  exit 1
-fi
 
 echo "$SERVICE_CONTENT" > /etc/systemd/system/server-security-manager.service
 
